c0593d0533f50a7fe648c8f7226e9612080405cc
moriharanariki <nariki@gmail.com>
2023-05-18 18:07:01 +0900

    fetchを使ってapiを叩くことに成功。

--- overseas_migration_media_nextjs/src/app/top/page.tsx
+++ overseas_migration_media_nextjs/src/app/top/page.tsx
@@ -1,7 +1,26 @@
-export default function Page() {
+
+type Book = {
+    id: number;
+    title: string;
+    body: string;
+}
+
+async function getData(){
+    const res = await fetch("http://localhost:3001/books",{
+        next: {
+            revalidate: 0,
+        },
+    });
+    return res.json();
+}
+export default async function Page() {
+    const books: Book[] = await getData();
     return (
         <>
-            <h1>テスト</h1>
+            <h1>apiテスト</h1>
+            {books.map((book) => (
+                <div key={book.id}>{book.title}</div>
+            ))}
         </>
     )
 }
