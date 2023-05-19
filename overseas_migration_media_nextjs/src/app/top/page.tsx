a48a2fa13771be9b7e3f732ae941064de190de54
moriharanariki <nariki@gmail.com>
2023-05-19 17:04:31 +0900

    headerの作成を行った。

--- overseas_migration_media_nextjs/src/app/top/page.tsx
+++ overseas_migration_media_nextjs/src/app/top/page.tsx
@@ -2,9 +2,9 @@ import {Header} from "@/app/components/organisms/Header";
 
 export default async function Page() {
     return (
-        <>
+        <div>
             <Header></Header>
             <h1>apiテスト</h1>
-        </>
+        </div>
     )
 }
