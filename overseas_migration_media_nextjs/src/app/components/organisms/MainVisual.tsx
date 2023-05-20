ab4614f00109b0bb0d934b1f6ccb03eceadc017d
moriharanariki <nariki@gmail.com>
2023-05-20 18:45:58 +0900

    メイン画像の挿入を行った。

---
+++ overseas_migration_media_nextjs/src/app/components/organisms/MainVisual.tsx
@@ -0,0 +1,8 @@
+
+export const MainVisual = () => {
+    return(
+        <div>
+            <img src="/image/alain-neptune-Lh8azYkQjcM-unsplash.jpg" alt="画像" className="1/5"/>
+        </div>
+    )
+}
--- overseas_migration_media_nextjs/src/app/top/page.tsx
+++ overseas_migration_media_nextjs/src/app/top/page.tsx
@@ -1,9 +1,11 @@
 import {Header} from "@/app/components/organisms/layout/Header";
+import {MainVisual} from "@/app/components/organisms/MainVisual";
 
 export default async function Page() {
     return (
         <div>
-            <Header></Header>
+            <Header/>
+            <MainVisual/>
         </div>
     )
 }
