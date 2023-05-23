4429dda6941ec91f4c194a6281fc689163e5b1f9
moriharanariki <nariki@gmail.com>
2023-05-23 19:26:05 +0900

    map画像と飛行機のアイコンを追加した。

--- overseas_migration_media_nextjs/src/app/top/page.tsx
+++ overseas_migration_media_nextjs/src/app/top/page.tsx
@@ -1,11 +1,13 @@
 import {Header} from "@/app/components/organisms/layout/Header";
 import {MainVisual} from "@/app/components/organisms/MainVisual";
+import {GlobalMap} from "@/app/components/atoms/GlobalMap";
 
 export default async function Page() {
     return (
         <div>
             <Header/>
             <MainVisual/>
+            <GlobalMap/>
         </div>
     )
 }
