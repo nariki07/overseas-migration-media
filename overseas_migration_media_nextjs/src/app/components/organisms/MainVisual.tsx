5a28fed4aea1c11ca5b8e546ac9d4aac55d43017
moriharanariki <nariki@gmail.com>
2023-05-23 17:31:34 +0900

    ヒーロイメージのサイズ調整を行った。

--- overseas_migration_media_nextjs/src/app/components/organisms/MainVisual.tsx
+++ overseas_migration_media_nextjs/src/app/components/organisms/MainVisual.tsx
@@ -1,8 +1,11 @@
 
 export const MainVisual = () => {
     return(
-        <div>
-            <img src="/image/alain-neptune-Lh8azYkQjcM-unsplash.jpg" alt="画像" className="1/5"/>
+        <div className="relative font-notoSans">
+            <img src="/image/alain-neptune-Lh8azYkQjcM-unsplash.png" alt="画像"
+                 className="h-auto"/>
+            <h1 className="text-4xl text-center text-white absolute inset-0 flex justify-center mt-7">Take off to a new world!!
+                <br/><span className="text-sm text-center text-white absolute inset-0 flex justify-center mt-11">CHANGE YOUR LIFE, CHANGE YOUR WORLD.</span></h1>
         </div>
     )
 }
--- overseas_migration_media_nextjs/tailwind.config.js
+++ overseas_migration_media_nextjs/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        notoSans:['Poppins']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
