c0d04de9c9184c2e6c9a3a707500b7e0c1e4771d
moriharanariki <nariki@gmail.com>
2023-06-10 12:06:30 +0900

    微調整

--- overseas_migration_media_nextjs/src/app/components/organisms/MapSearch.tsx
+++ overseas_migration_media_nextjs/src/app/components/organisms/MapSearch.tsx
@@ -1,16 +1,14 @@
-import {ReactNode} from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faMessage} from "@fortawesome/free-solid-svg-icons";
-import {GlobalMap} from "@/app/components/atoms/GlobalMap";
-import {SpeechBubble} from "@/app/components/atoms/SpeechBubble";
+import { GlobalMap } from "@/app/components/atoms/GlobalMap";
+import { SpeechBubble } from "@/app/components/atoms/SpeechBubble";
 
 export const MapSearch = () => {
-    return (
-        <div className="relative flex justify-center items-center">
-          <GlobalMap />
-          <div className="absolute top-20 left-80">
-              <SpeechBubble>ヨーロッパ</SpeechBubble>
-            </div>
-        </div>
-    );
+  return (
+    <div className="relative">
+      <GlobalMap />
+      <div className="absolute top-10 left-20">
+        <SpeechBubble>ヨーロッパ</SpeechBubble>
+      </div>
+    </div>
+  );
 };
+a;
